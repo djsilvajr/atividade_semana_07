4082389549a434cb9e081cdb44fd6b919d3b003b
Douglas Junior <douglasjr0809@gmail.com>
2022-08-20 16:50:03 -0300

    exercicio 3

--- Restaurante/src/main.jsx
+++ Restaurante/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Main from './Main-div'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* Header */}
     <App />
+    <Main />
   </React.StrictMode>
 )
